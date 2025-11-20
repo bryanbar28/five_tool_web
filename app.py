61753d6348008648a521b46b474726bed7183e5f
bryan <bbarrera@lcinnovation.org>
2025-11-20 12:11:55 -0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 import pandas as pd
 import streamlit as st
 import plotly.express as px
-import OpenAI
+import OpenAI from openai 
 from googleapiclient.discovery import build
 
 # -------------------------------
